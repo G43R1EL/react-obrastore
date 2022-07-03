bfeb805fb22974533f7050210c817b37d0070b6a
Nelson Gabriel Hernández <gabrielymarg@gmail.com>
2022-07-03 01:17:15 -0300

    addDoc... Order creation... Buyer data...

--- src/components/Main/Cart/Cart.jsx
+++ src/components/Main/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../../context/CartContext/CartContext';
 import './Cart.css';
+import Checkout from './Checkout/Checkout';
 import ProductLine from './ProductLine/ProductLine';
 
 function Cart () {
@@ -24,16 +25,21 @@ function Cart () {
                     </>
                 :
                     <>
-                        <h1>Contenido del carro</h1>
-                        {
-                            cart.map(product=>{
-                                return (
-                                    <ProductLine product={product} key={product.id} />
-                                );
-                            })
-                        }
-                        <div className="cart__total">
-                            <b>TOTAL:</b><span> {pesoArg.format(cartTotal()) + '-'}</span>
+                        <div className='cart__container'>
+                            <div className='cart__details'>
+                                <h2>Contenido del carro</h2>
+                                {
+                                    cart.map(product=>{
+                                        return (
+                                            <ProductLine product={product} key={product.id} />
+                                        );
+                                    })
+                                }
+                                <div className="cart__total">
+                                    <b>TOTAL:</b><span> {pesoArg.format(cartTotal()) + '-'}</span>
+                                </div>
+                            </div>
+                            <Checkout cart={cart} />
                         </div>
                     </>
             }
